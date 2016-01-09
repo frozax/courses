eb96ac6dc7e85ece3584c13999f1bcc6251bc4f4
Francois Guibert <frozax@frozax.com>
2016-01-09 13:09:30 +0100

    set working dir to app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from controller import Controller
 from model import Model
 
 def main():
+    import os
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
     m = Model()
     v = View()
     c = Controller(m, v)
