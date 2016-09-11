e292ce2eb3d45da75665205588c47f04e8890471
Francois Guibert <frozax@frozax.com>
2016-09-11 21:25:27 +0200

    fix paths

--- web/server.py
+++ web/server.py
@@ -1,8 +1,11 @@
+import os
 from bottle import route, run, SimpleTemplate, static_file
 from model import Model
 
+PATH = os.path.dirname(os.path.realpath(__file__))
+
 def tpl(file_name, **kwargs):
-    template = SimpleTemplate(open(file_name + ".tpl").read())
+    template = SimpleTemplate(open(os.path.join(PATH, file_name + ".tpl")).read())
     return template.render(
         **kwargs
     )
@@ -20,7 +23,7 @@ def courses():
 
 @route('/static/<path:path>')
 def static(path):
-    return static_file(path, root='static/')
+    return static_file(path, root=os.path.join(PATH, 'static/'))
 
 def run_server():
     run(host='0.0.0.0', port=8080, debug=True, reloader=True)
